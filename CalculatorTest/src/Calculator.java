418654549828fb190f1293794833776f875cccc3
duongmoe174 <duongmoe174@gmail.com>
2022-02-17 10:06:19 +0700

    up Calculator Test

---
+++ CalculatorTest/src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static int add(int first, int second){
+        int resutlAdd = first + second;
+        return resutlAdd;
+    }
+
+    public static int sub(int first, int second){
+        int resultSub = first - second;
+        return resultSub;
+    }
+}
